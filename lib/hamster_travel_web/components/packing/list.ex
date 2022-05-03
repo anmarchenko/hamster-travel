584ed95d2c2cc4b406a88628663f3bbb0f46c8ce
Andrey Marchenko <andrei.marchenko@protonmail.com>
2022-05-03 22:06:02 +0200

    dark mode for backpacks

--- lib/hamster_travel_web/components/packing/list.ex
+++ lib/hamster_travel_web/components/packing/list.ex
@@ -18,14 +18,14 @@ defmodule HamsterTravelWeb.Packing.List do
       <span>
         <UI.card>
           <div class="flex flex-col w-full">
-            <div class="p-4 bg-violet-700 text-white rounded-t-lg">
+            <div class="p-4 bg-violet-700 dark:bg-violet-900 text-white dark:text-zinc-400 rounded-t-lg">
               <%= @list.name %>
             </div>
             <div class="px-4 py-2">
               <%= for item <- @list.items do %>
                 <div class="mt-2">
                   <%= label class: "cursor-pointer inline-flex items-center block gap-2" do %>
-                    <%= checkbox(:items, "item-#{item.id}", value: item.checked, class: "accent-violet-700 border-gray-300 rounded w-5 h-5 ease-linear transition-all duration-150 dark:bg-gray-800 dark:border-gray-300") %>
+                    <%= checkbox(:items, "item-#{item.id}", value: item.checked, class: "text-violet-700 border-gray-300 rounded w-5 h-5 ease-linear transition-all duration-150 dark:bg-gray-800 dark:border-gray-300") %>
                     <div class="text-sm block"><%= item.name %></div>
                   <% end %>
                 </div>
