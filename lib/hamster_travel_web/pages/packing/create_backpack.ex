979f5e2a5f3f1c8d729b6324ef1774b4db5dcab8
Andrey Marchenko <andrei.marchenko@protonmail.com>
2023-01-16 16:04:27 +0100

    crate backpack form - automatically change nights when days change

--- lib/hamster_travel_web/pages/packing/create_backpack.ex
+++ lib/hamster_travel_web/pages/packing/create_backpack.ex
@@ -4,6 +4,8 @@ defmodule HamsterTravelWeb.Packing.CreateBackpack do
   """
   use HamsterTravelWeb, :live_view
 
+  alias Ecto.Changeset
+
   alias HamsterTravel.Packing
 
   @impl true
@@ -34,6 +36,18 @@ defmodule HamsterTravelWeb.Packing.CreateBackpack do
 
       {:error, changeset} ->
         {:noreply, assign(socket, %{changeset: changeset})}
+    end
+  end
+
+  @impl true
+  def handle_event("days-changed", %{"backpack" => %{"days" => days}}, socket) do
+    {days, _} = Integer.parse(days)
+
+    if days > 1 do
+      changeset = socket.assigns.changeset |> Changeset.put_change(:nights, days - 1)
+      {:noreply, assign(socket, %{changeset: changeset})}
+    else
+      {:noreply, socket}
     end
   end
 end
