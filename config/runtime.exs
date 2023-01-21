cc953aa81dbed9ca2756faf009f9a9f9fd6801bb
Andrey Marchenko <andrei.marchenko@protonmail.com>
2023-01-21 17:55:18 +0100

    maybe now?

--- config/runtime.exs
+++ config/runtime.exs
@@ -33,13 +33,15 @@ if config_env() == :prod do
       """
 
   config :hamster_travel, HamsterTravelWeb.Endpoint,
+    server: true,
+    url: [host: System.get_env("PHX_HOST"), port: 80],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: String.to_integer(System.get_env("PORT") || "4000")
+      port: 4000
     ],
     secret_key_base: secret_key_base
 
