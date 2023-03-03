9c54859c231b3a986ed456414d00b3dfba49373e
Andrey Marchenko <andrei.marchenko@protonmail.com>
2023-03-03 07:34:23 +0100

    adapt seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,10 +10,30 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
+admin1 =
+  HamsterTravel.Repo.insert!(%HamsterTravel.Accounts.User{
+    name: "Test Admin",
+    email: "admin@mail.test",
+    confirmed_at: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second),
+    hashed_password: Bcrypt.hash_pwd_salt("test1234"),
+    locale: "ru"
+  })
+
+admin2 =
+  HamsterTravel.Repo.insert!(%HamsterTravel.Accounts.User{
+    name: "Test Admin 2",
+    email: "admin2@mail.test",
+    confirmed_at: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second),
+    hashed_password: Bcrypt.hash_pwd_salt("test1234"),
+    locale: "ru"
+  })
+
 HamsterTravel.Repo.insert!(%HamsterTravel.Accounts.User{
-  name: "Test Admin",
-  email: "admin@mail.test",
+  name: "Test Admin 3",
+  email: "admin3@mail.test",
   confirmed_at: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second),
   hashed_password: Bcrypt.hash_pwd_salt("test1234"),
   locale: "ru"
 })
+
+HamsterTravel.Social.add_friends(admin1.id, admin2.id)
