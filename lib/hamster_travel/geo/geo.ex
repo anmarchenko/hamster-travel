2bc47e66a43e7cfdb669ac44d00368ab2eb0ac9f
Andrey Marchenko <andrei.marchenko@protonmail.com>
2024-08-05 19:47:03 +0200

    more functions to query countries

--- lib/hamster_travel/geo/geo.ex
+++ lib/hamster_travel/geo/geo.ex
@@ -22,6 +22,13 @@ defmodule HamsterTravel.Geo do
   end
 
   @doc """
+  Returns the list of country ISO codes.
+  """
+  def list_country_iso_codes do
+    Repo.all(from c in Country, select: c.iso, order_by: c.iso)
+  end
+
+  @doc """
   Gets a single country.
 
   Raises `Ecto.NoResultsError` if the Country does not exist.
@@ -36,4 +43,12 @@ defmodule HamsterTravel.Geo do
 
   """
   def get_country!(id), do: Repo.get!(Country, id)
+
+  def find_country_by_geonames_id(geonames_id) do
+    Repo.get_by(Country, geonames_id: geonames_id)
+  end
+
+  def find_country_by_iso(iso) do
+    Repo.get_by(Country, iso: iso)
+  end
 end
--- test/hamster_travel/geo/geo_test.exs
+++ test/hamster_travel/geo/geo_test.exs
@@ -5,6 +5,8 @@ defmodule HamsterTravel.GeoTest do
   alias HamsterTravel.Geo
 
   describe "countries" do
+    alias HamsterTravel.Geo.Country
+
     setup do
       geonames_countries_fixture()
 
@@ -18,6 +20,28 @@ defmodule HamsterTravel.GeoTest do
     test "get_country!/1 returns the country with given id" do
       [country | _] = Geo.list_countries()
       assert Geo.get_country!(country.id) == country
+    end
+
+    test "find_country_by_geonames_id returns the country with given geonames_id" do
+      [country | _] = Geo.list_countries()
+      assert Geo.find_country_by_geonames_id(country.geonames_id) == country
+    end
+
+    test "find_country_by_geonames_id returns nil if the country with given geonames_id does not exist" do
+      assert Geo.find_country_by_geonames_id("999999999") == nil
+    end
+
+    test "find_country_by_iso returns the country with given iso" do
+      assert %Country{name: "Germany"} = Geo.find_country_by_iso("DE")
+    end
+
+    test "find_country_by_iso returns nil if the country with given iso does not exist" do
+      assert Geo.find_country_by_iso("ZZ") == nil
+    end
+
+    test "list_country_iso_codes/0 returns all country ISO codes ordered alphabetically" do
+      assert Enum.count(Geo.list_country_iso_codes()) == 246
+      assert ["AD", "AE", "AF"] == Enum.take(Geo.list_country_iso_codes(), 3)
     end
   end
 end
