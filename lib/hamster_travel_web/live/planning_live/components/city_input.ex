f24b3ee5e45b1e52c2b08082865ca598e76b8cbd
Andrey Marchenko <andrei.marchenko@protonmail.com>
2024-12-02 19:08:49 +0100

    the component's skeleton

---
+++ lib/hamster_travel_web/live/planning_live/components/city_input.ex
@@ -0,0 +1,34 @@
+defmodule HamsterTravelWeb.Planning.CityInput do
+  use HamsterTravelWeb, :live_component
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    """
+  end
+
+  def search_input(assigns) do
+    ~H"""
+    """
+  end
+
+  def results(assigns) do
+    ~H"""
+    """
+  end
+
+  def result_item(assigns) do
+    ~H"""
+    """
+  end
+
+  def search_modal(assigns) do
+    ~H"""
+    """
+  end
+
+  @impl true
+  def update(assigns, socket) do
+    {:ok, socket}
+  end
+end
