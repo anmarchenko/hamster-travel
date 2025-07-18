1d623aebbc969462a34c57c2ccc4d93f20ea204a
Andrey Marchenko <andrei.marchenko@protonmail.com>
2025-07-18 23:45:08 +0300

    add and configure tidewave mcp server for claude

--- lib/hamster_travel_web/endpoint.ex
+++ lib/hamster_travel_web/endpoint.ex
@@ -23,6 +23,10 @@ defmodule HamsterTravelWeb.Endpoint do
     gzip: true,
     only: HamsterTravelWeb.static_paths()
 
+  if Code.ensure_loaded?(Tidewave) do
+    plug Tidewave
+  end
+
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
--- mix.exs
+++ mix.exs
@@ -98,7 +98,8 @@ defmodule HamsterTravel.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:floki, ">= 0.30.0", only: :test},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:live_debugger, "~> 0.3.0", only: :dev}
+      {:live_debugger, "~> 0.3.0", only: :dev},
+      {:tidewave, "~> 0.2", only: :dev}
     ]
   end
 
